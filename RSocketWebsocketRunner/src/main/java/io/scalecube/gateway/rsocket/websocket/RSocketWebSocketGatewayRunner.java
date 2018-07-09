88f9b08ce037ad73725d239bc93b55f9fe65a67b
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2018-07-09 10:13:55 +0300

    Get seeds from config registry

--- RSocketWebsocketRunner/src/main/java/io/scalecube/gateway/rsocket/websocket/RSocketWebSocketGatewayRunner.java
+++ RSocketWebsocketRunner/src/main/java/io/scalecube/gateway/rsocket/websocket/RSocketWebSocketGatewayRunner.java
@@ -1,15 +1,26 @@
 package io.scalecube.gateway.rsocket.websocket;
 
+import io.scalecube.config.ConfigRegistry;
+import io.scalecube.gateway.config.GatewayConfigRegistry;
 import io.scalecube.services.Microservices;
 import io.scalecube.transport.Address;
 
+import java.util.Collections;
+import java.util.List;
+
 public class RSocketWebSocketGatewayRunner {
 
-  private static final Address SEED_ADDRESS = Address.from("localhost:4801");
+  private static final String SEEDS = "SEEDS";
+  private static final List<String> DEFAULT_SEEDS = Collections.singletonList("localhost:4802");
 
   public static void main(String[] args) throws InterruptedException {
+    final ConfigRegistry configRegistry = GatewayConfigRegistry.configRegistry();
+
+    final Address[] seeds = configRegistry.stringListValue(SEEDS, DEFAULT_SEEDS)
+        .stream().map(Address::from).toArray(Address[]::new);
+
     Microservices seed = Microservices.builder()
-        .seeds(SEED_ADDRESS)
+        .seeds(seeds)
         .startAwait();
 
     RSocketWebsocketGateway gateway = new RSocketWebsocketGateway(seed);
